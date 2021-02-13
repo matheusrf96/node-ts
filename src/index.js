871e0d806d340a1e0949a17683ac37e2539556d0
Matheus Rodrigues <matheusrf96@gmail.com>
2021-02-12 21:10:40 -0300

    Create and update projects

--- src/index.js
+++ src/index.js
@@ -1,37 +1,53 @@
 const express = require('express')
+const { uuid } = require('uuidv4')
 
 const app = express()
 
 app.use(express.json())
 
+const projects = []
+
 app.get('/projects', (req, res) => {
     const { title, author } = req.query
 
-    console.log(title, author)
-
-    return res.json({
-        "message": "Success :)",
-    })
+    return res.json(projects)
 })
 
 app.post('/projects', (req, res) => {
-    const body = req.body
+    const { title, author } = req.body
 
-    console.log(body)
+    const id = uuid()
+    const project = {
+        id,
+        title,
+        author
+    }
 
-    return res.json({
-        "message": "Success :)",
-    })
+    projects.push(project)
+
+    return res.json(project)
 })
 
 app.put('/projects/:id', (req, res) => {
     const { id } = req.params
+    const { title, author } = req.body
+    const projectIndex = projects.findIndex(project => project.id == id)
 
-    console.log(id)
+    if(projectIndex < 0) {
+        return res.status(404).json({
+            "message": "ID not found :(",
+        })
+    }
 
-    return res.json({
-        "message": "Success :)",
-    })
+    const project = {
+        id,
+        title,
+        author
+    }
+
+    projects[projectIndex] = project
+
+    return res.json(project)
 })
 
 app.listen(3000, () => {
